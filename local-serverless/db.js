a40d4b0cc6d40f39ab1620456b577077e410dd30
shankalpa-hnl <shankalpa@hatsandladders.com>
2022-03-09 19:17:17 +0545

    server part of the app

--- local-serverless/db.js
+++
@@ -1,9 +0,0 @@
-const {DynamoDBClient} = require('@aws-sdk/client-dynamodb');
-const client = new DynamoDBClient({
-    region: "us-west-2",
-    accessKeyId: "access_key_id",
-    secretAccessKeyId: "secret_access_key",
-    endpoint: "http://localhost:8000"
-});
-
-module.exports = client;
